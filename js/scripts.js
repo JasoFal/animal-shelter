cede3f105b14a4d50d4e5b2b39b3d2d9ffab17fa
Jason Falk <jasonfalk9@gmail.com>
2022-12-07 09:59:56 -0800

    Clean up dry code.

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,19 @@ window.onload = function () {
     max = Math.floor(255);
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
-  function random2(min, max) {
-    min = Math.ceil(0);
-    max = Math.floor(255);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  function random3(min, max) {
-    min = Math.ceil(0);
-    max = Math.floor(255);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
 
   let body = document.querySelector(`body`);
 
   body.onmouseover = function () {
-    body.style.backgroundColor = `rgb(${random1()}, ${random2()}, ${random3()})`;
+    body.style.backgroundColor = `rgb(${random1()}, ${random1()}, ${random1()})`;
   }
 };
-// let p = document.querySelector("p");
-//   p.onmouseover = function() {
-//   document.querySelector("p>em").innerText = "Don't be surprised";
-// };
+
+//Example for setting background color on reload.
+
+  // let bgColor1 = random1();
+  // let bgColor2 = random1();
+  // let bgColor3 = random1();
+  // let rgbRand = `rgb(${bgColor1}, ${bgColor2}, ${bgColor3})`;
+
+  // body.style.backgroundColor = rgbRand;
